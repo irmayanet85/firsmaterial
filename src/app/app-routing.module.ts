127e196ff0bffc001d953f548cd2196cb911eb27
Irma Cabrera <irmayanet85@gmail.com>
2023-02-24 14:38:11 -0500

    antes de modificar el profile

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,12 +2,6 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 import { AuthRoutingModule } from './auth/auth-routing.module';
-
-import { CommonModule } from '@angular/common';
-import { AuthModule } from './auth/auth.module';
-import { LoginComponent } from './auth/login/login.component';
-
-
 import { PagesRoutingModule } from './pages/pages-routing.module';
 import { NopagefoundComponent } from './nopagefound/nopagefound.component';
 
